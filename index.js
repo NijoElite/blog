6d40713b40265ae96b681b58501b919d27809c4b
Eugene Malchenko <nijoelite@gmail.com>
2019-06-03 22:49:54 +0300

    Init entry point

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express'),
+      mongoose = require('mongoose'),
+      config = require('./config');
+
+const app = express();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+
+/// mongoose
+mongoose.connect(config.mongodb_uri);
+mongoose.set('debug', !isProduction);
+
+// models
+require('./models/Article');
+require('./models/User');
+
+/// routes
+app.use(require('./routes'));
+
+// 404
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+    app.use((err, req, res) => {
+        console.log(err.stack);
+
+        res.status(err.status || 500);
+
+        res.json({'errors': {
+                message: err.message,
+                error: err
+        }});
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.json({'errors': {
+            message: err.message,
+            error: {}
+    }});
+});
+
+
+// start server
+const server = app.listen(process.env.PORT || 3000, () =>{
+    console.log('Listening on port ' + server.address().port);
+});
+
+
