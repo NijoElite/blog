bb65a5551a034b16f6c6f12cccd2166c060b5be6
Eugene Malchenko <nijoelite@gmail.com>
2019-06-03 23:42:53 +0300

    Add tags API

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -1,4 +1,19 @@
-const router = require('express').Router();
+const router = require('express').Router(),
+    mongoose = require('mongoose'),
+    Article = mongoose.model('Article');
+
+router.get('/', function(req, res) {
+    Article.aggregate([{
+        $unwind: "$tagList"
+    },
+    {
+        $group: {
+            _id: "$tagList",
+            count: { $sum: 1 }
+        }
+    }
+    ]).then(r => res.send(r));
+});
 
 
 module.exports = router;
