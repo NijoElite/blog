089131104c5c3ae1bc3689aba11a6113730d9f6a
Eugene Malchenko <nijoelite@gmail.com>
2019-06-03 09:23:48 +0300

    Update config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,4 @@
-process.env.NODE_ENV = 'debug';
-process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017';
-
-
 module.exports = {
-    secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+    secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
+    mongodb_uri: process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017'
 };
