626478ed534045d2a53dfd30be7dd4e35441f843
Eugene Malchenko <nijoelite@gmail.com>
2019-06-03 09:22:23 +0300

    Init Article model

---
+++ models/Article.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose'),
+      uniqueValidator = require('mongoose-unique-validator');
+
+const ArticleSchema = new mongoose.Schema({
+    slug: {type: String, lowercase: true, unique: true},
+    title: String,
+    description: String,
+    body: String,
+    tagList: [{type: String}],
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, { timestamps: true });
+
+ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'});
+
+ArticleSchema.pre('validate', (next) => {
+    if(!this.slug)  {
+        this.slugify();
+    }
+
+    next();
+});
+
+ArticleSchema.methods.slugify = () => {
+    this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36);
+};
+
+mongoose.model('Article', ArticleSchema);
