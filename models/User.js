2cf7ff823a7c83c0fc6dae466293435370902748
Eugene Malchenko <nijoelite@gmail.com>
2019-06-03 22:47:23 +0300

    Init User model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose'),
+      uniqueValidator = require('mongoose-unique-validator'),
+      crypto = require('crypto');
+
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"],
+                              match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
+
+    email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"],
+                           match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
+
+    bio: String,
+    image: String,
+    hash: String,
+    salt: String
+}, { timestamps: true });
+
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+
+/// Password
+
+UserSchema.methods.validatePassword = function(pass) {
+    return this.hash === crypto.pbkdf2Sync(pass, this.salt, 10000, 512, 'sha512');
+};
+
+
+UserSchema.methods.setPassword = function(pass) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(pass, this.salt, 10000, 512, 'sha512');
+};
+
+mongoose.model('User', UserSchema);
