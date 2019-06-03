a85d03bb8ea597caab7b3412df54687662e40f35
Eugene Malchenko <nijoelite@gmail.com>
2019-06-03 22:15:42 +0300

    Refactor articles API

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -2,28 +2,18 @@ const router = require('express').Router(),
       mongoose = require('mongoose'),
       Article = mongoose.model('Article');
 
-// Preload article objects on routes with ':article'
-router.param('slug', function(req, res, next, slug) {
-    Article.findOne({ slug: slug})
-        .populate('author')
-        .then(function (article) {
-            if (!article) { return res.sendStatus(404); }
-
-            req.article = article;
-
-            return next();
-        }).catch(next);
-});
 
 // Get The Article
 router.get('/articles/:slug', (req, res) => {
     Article.findOne({slug: req.params.slug}).then(function(article) {
+        if (!article) { return res.sendStatus(404); }
+
         res.send(article);
     });
 });
 
 // Get Articles
-// offset = Number[0;100], limit = Number, Author = String, Tags = tag-1, tag-2, tag-3
+// offset = Number >= 0, limit = Number[0;100], Author = String, Tags = tag-1, tag-2, tag-3
 router.get('/articles', (req, res) => {
     const q = req.query;
     const query = {};
