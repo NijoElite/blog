75960410fc8d186d56288a909476f4b608c9c40e
Eugene Malchenko <nijoelite@gmail.com>
2019-06-04 04:26:19 +0300

    Update dependencies

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,5 +8,6 @@ router.use('/articles', require('./articles'));
 router.use('/tags', require('./tags'));
 router.use('/users', require('./users'));
 router.use('/reg', require('./reg'));
+router.use('/login', require('./auth'));
 
 module.exports = router;
